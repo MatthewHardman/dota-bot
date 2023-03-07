821b1e5aa4704e13eaa8fd7dfa7880f1d63bb0c3
MatthewHardman <matthew9016@gmail.com>
2023-03-07 15:25:40 -0500

    role debug

--- commands/dota.js
+++ commands/dota.js
@@ -27,7 +27,7 @@ module.exports = {
       (role) => role.id === "1071259658943217745"
     );
     console.log(role);
-    console.log(role[Guild]);
+    //console.log(role[Guild]);
     console.log(role.Guild);
     const message = await interaction.reply({
       content: ` Hello ${role.id}, ${interaction.user.username} would like to play with a stack size of ${stackSize} and is willing to wait ${timeOutInMin} minutes! Please react if you'd like to be pinged when a stack forms. If you initiated the command, I have reacted for you.`,
