d1ef3bf6e47b8a02e74e7d97db2da576d3093767
MatthewHardman <matthew9016@gmail.com>
2023-03-07 10:41:35 -0500

    fixed role call

--- commands/dota.js
+++ commands/dota.js
@@ -23,7 +23,9 @@ module.exports = {
     let stackSize = interaction.options.getInteger("stacksize");
     let timeOutInMin = interaction.options.getInteger("timeout");
     let timeOutInMS = timeOutInMin * 60000;
-    const role = await interaction.guild.role.fetch("071259658943217745");
+    const role = interaction.guild.role.cache.filter(
+      (role) => (role.id = "071259658943217745")
+    );
     const message = await interaction.reply({
       content: ` Hello ${role}, ${interaction.user.username} would like to play with a stack size of ${stackSize} and is willing to wait ${timeOutInMin} minutes! Please react if you'd like to be pinged when a stack forms. If you initiated the command, I have reacted for you.`,
       fetchReply: true,
