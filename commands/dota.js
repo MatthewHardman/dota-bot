5ebe4077003dee604a4e021f9cdb735fe4221512
MatthewHardman <matthew9016@gmail.com>
2023-03-07 16:01:38 -0500

    role debugging

--- commands/dota.js
+++ commands/dota.js
@@ -23,12 +23,10 @@ module.exports = {
     let stackSize = interaction.options.getInteger("stacksize");
     let timeOutInMin = interaction.options.getInteger("timeout");
     let timeOutInMS = timeOutInMin * 60000;
-    const role = interaction.guild.roles.cache.filter(
+    const role = interaction.guild.roles.cache.find(
       (role) => role.id === "1071259658943217745"
     );
     console.log(role);
-    //console.log(role[Guild]);
-    console.log(role.Guild);
     const message = await interaction.reply({
       content: ` Hello ${role.id}, ${interaction.user.username} would like to play with a stack size of ${stackSize} and is willing to wait ${timeOutInMin} minutes! Please react if you'd like to be pinged when a stack forms. If you initiated the command, I have reacted for you.`,
       fetchReply: true,
