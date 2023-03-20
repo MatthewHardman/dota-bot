670beb0471758fccc56b297737219f491f1bb895
Mike <mcarmody2013@gmail.com>
2023-03-20 13:33:13 -0400

    split opt commands into two files

--- commands/optout.js
+++ commands/optout.js
@@ -1,10 +1,10 @@
-// optin.js
+// optout.js
 const { SlashCommandBuilder, CommandInteraction } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("optin")
-    .setDescription("Opt-in to the dooters role"),
+    .setName("optout")
+    .setDescription("Opt-out of the dooters role"),
 
   async execute(interaction) {
     const dootersRole = interaction.guild.roles.cache.find(role => role.name.toLowerCase() === 'dooters');
@@ -14,12 +14,12 @@ module.exports = {
       return;
     }
 
-    if (interaction.member.roles.cache.has(dootersRole.id)) {
-      await interaction.reply("You already have the 'dooters' role.");
+    if (!interaction.member.roles.cache.has(dootersRole.id)) {
+      await interaction.reply("You don't have the 'dooters' role.");
       return;
     }
 
-    await interaction.member.roles.add(dootersRole);
-    await interaction.reply("You have been given the 'dooters' role.");
+    await interaction.member.roles.remove(dootersRole);
+    await interaction.reply("You have been removed from the 'dooters' role.");
   },
 };
