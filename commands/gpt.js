e6b026aa3b277a6735beafb50e02ef5a4fe2c469
MatthewHardman <matthew9016@gmail.com>
2023-03-20 17:22:23 -0400

    fixed api key

--- commands/gpt.js
+++ commands/gpt.js
@@ -1,21 +1,22 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { Configuration, OpenAIApi } = require('openai');
+const { SlashCommandBuilder } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const configuration = new Configuration({
-  apiKey: 'your_openai_api_key_here',
+  apiKey: OPENAI_API_KEY,
 });
 
 async function getInfo(query) {
   const openai = new OpenAIApi(configuration);
   const completion = await openai.createCompletion({
-    model: 'gpt-4-32k',
+    model: "gpt-4-32k",
     prompt: query,
     max_tokens: 1024,
     n: 1,
     stop: null,
     temperature: 0.7,
   });
-
 
   const assistantMessage = completion.data.choices[0].text;
   return assistantMessage;
@@ -24,19 +25,22 @@ async function getInfo(query) {
 module.exports = {
   data: new SlashCommandBuilder()
 
-    .setName('gpt4')
-    .setDescription('Get information using GPT-4 API')
+    .setName("gpt4")
+    .setDescription("Get information using GPT-4 API")
     .addStringOption((option) =>
       option
-        .setName('query')
-        .setDescription('The query you want to ask GPT-4 API')
+        .setName("query")
+        .setDescription("The query you want to ask GPT-4 API")
         .setRequired(true)
     ),
   async execute(interaction) {
-    const botDevRole = interaction.guild.roles.cache.find((role) => role.name === 'Bot Dev');
+    const botDevRole = interaction.guild.roles.cache.find(
+      (role) => role.name === "Bot Dev"
+    );
     if (!interaction.member.roles.cache.has(botDevRole.id)) {
       await interaction.reply({
-        content: 'You do not have the required role (Bot Dev) to use this command.',
+        content:
+          "You do not have the required role (Bot Dev) to use this command.",
 
         ephemeral: true,
       });
@@ -49,8 +53,9 @@ module.exports = {
     if (result) {
       await interaction.reply(result);
     } else {
-      await interaction.reply('Sorry, I could not find any information for that query.');
-
+      await interaction.reply(
+        "Sorry, I could not find any information for that query."
+      );
     }
   },
 };
