9d70b62b552d9e2d5928063a39cc9fc05ed78ed1
Mike <mcarmody2013@gmail.com>
2023-03-24 18:52:04 -0400

    Revert "Update server.js"
    This reverts commit 3200ed8316d59b03f4df977dd87f414cfa4e96c1.

--- commands/server.js
+++ commands/server.js
@@ -5,38 +5,8 @@ module.exports = {
     .setName("server")
     .setDescription("Display info about this server."),
   async execute(interaction) {
-
-    // Check if the user has the Bot Dev role
-    const botDevRole = interaction.guild.roles.cache.find(
-      (role) => role.name === "Bot Dev"
+    return interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
     );
-    if (!interaction.member.roles.cache.has(botDevRole.id)) {
-      await interaction.reply({
-        content:
-          "You do not have the required role (Bot Dev) to use this command.",
-        ephemeral: true,
-      });
-      return;
-    }
-
-    // Defer the reply
-    await interaction.deferReply();
-
-    // Execute the deploy-commands.js script
-    exec("node ./deploy-commands.js", (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing the script: ${error.message}`);
-        interaction.editReply(
-          "An error occurred while executing the deploy-commands.js script. Please check the console for more information."
-        );
-        return;
-      }
-
-      interaction.editReply("The deploy-commands.js script has been executed successfully.");
-    });
-
-    // return interaction.reply(
-    //   `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    // );
-  },   
+  },
 };
