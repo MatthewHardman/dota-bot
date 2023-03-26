7f105109b21f2c5af71a5c5c3778d247f9c3bd7f
Mike <mcarmody2013@gmail.com>
2023-03-25 20:49:58 -0400

    demo embed cause it wasn't working

--- commands/wskbosch.js
+++ commands/wskbosch.js
@@ -42,22 +42,51 @@ module.exports = {
     try {
       const imageUrl = await getImage(prompt);
 
-      await interaction.editReply({ embeds: {
-        color: 0x0099ff,
-        title: prompt,
+      await interaction.editReply({ embeds: [{
+        color: 3447003,
         author: {
-          name: interaction.member.displayName,
-          icon_url: interaction.member.avatar,
+          name: "Author Name, it can hold 256 characters",
+          icon_url: "https://i.imgur.com/lm8s41J.png"
         },
-        description: 'Art created by WSKBosch and the author.',
         thumbnail: {
-          url: imageUrl,
+          url: "http://i.imgur.com/p2qNFag.png"
         },
         image: {
-          url: imageUrl,
+          url: "http://i.imgur.com/yVpymuV.png"
         },
-        timestamp: new Date().toISOString(),
-      }});
+        title: "This is your title, it can hold 256 characters",
+        url: "https://discord.js.org/#/docs/main/master/class/MessageEmbed",
+        description: "This is the main body of text, it can hold 2048 characters.",
+        fields: [{
+          name: "This is a single field title, it can hold 256 characters",
+          value: "This is a field value, it can hold 1024 characters.",
+          inline: false
+        },
+        {
+          name: "Inline fields",
+          value: "They can have different fields with small headlines, and you can inline them.",
+          inline: true
+        },
+        {
+          name: "Masked links",
+          value: "You can put [masked links](https://discord.js.org/#/docs/main/master/class/MessageEmbed) inside of rich embeds.",
+          inline: true
+        },
+        {
+          name: "Markdown",
+          value: "You can put all the *usual* **__Markdown__** inside of them.",
+          inline: true
+        },
+        {
+          name: "\u200b",
+          value:"\u200b"
+        }],
+        timestamp: new Date(),
+        footer: {
+          icon_url: "http://i.imgur.com/w1vhFSR.png",
+          text: "This is the footer text, it can hold 2048 characters"
+        }
+      }]});
     } catch (error) {
       console.error(`Error while calling OpenAI Images API: ${error.message}`);
       await interaction.editReply(
